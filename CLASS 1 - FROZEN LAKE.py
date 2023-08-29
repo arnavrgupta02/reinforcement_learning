326bce436448556d2d542aa4bd2c37cee71901fb
Arnav Gupta <109815755+arnavrgupta02@users.noreply.github.com>
2023-08-29 15:27:59 +0530

    Add files via upload

--- CLASS 1 - FROZEN LAKE.py
+++ CLASS 1 - FROZEN LAKE.py
@@ -4,4 +4,19 @@ Created on Tue Aug 29 14:41:13 2023
 
 @author: ADMIN
 """
+import gymnasium as gym
+import time
+env = gym.make('FrozenLake-v1',render_mode='human')
 
+t_s = 20
+
+env.reset()
+time.sleep(2)
+print("Time Step : 0")
+env.render()
+for t in range(t_s):
+    random_action = env.action_space.sample()
+    next_state,reqard,done,info,prob=env.step(random_action)
+    print("Time Step {}".format(t+1))
+    if(done):
+        break
