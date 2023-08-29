7c08ce54ede3bc0933e0da49b353ccdf85af52d6
Arnav Gupta <109815755+arnavrgupta02@users.noreply.github.com>
2023-08-29 15:50:24 +0530

    Add files via upload

--- CLASS 1 - FROZEN LAKE.py
+++ CLASS 1 - FROZEN LAKE.py
@@ -9,14 +9,23 @@ import time
 env = gym.make('FrozenLake-v1',render_mode='human')
 
 t_s = 20
-
-env.reset()
-time.sleep(2)
-print("Time Step : 0")
-env.render()
-for t in range(t_s):
-    random_action = env.action_space.sample()
-    next_state,reward,done,info,prob=env.step(random_action)
-    print("Time Step {}".format(t+1))
-    if(done):
-        break
+eps = 100
+l_actions=[]
+i=0
+while(i<eps):
+    print("eps ",i+1,"\n\n")
+    env.reset()
+    time.sleep(2)
+    print("Time Step : 0")
+    env.render()
+    for t in range(t_s):
+        random_action = env.action_space.sample()
+        next_state,reward,done,info,prob=env.step(random_action)
+        #l_action[ep].append()
+        print("Time Step {}".format(t+1))
+        if(done):
+            if(reward == 1):
+                print("good work")
+                i = eps+1
+            break
+    i+=1
